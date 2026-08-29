6009e7d28d57ca6f175450d23b9356887413b0e7
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-08-29 14:17:34 +0530

    Time: 1 ms (83.74%), Space: 43.9 MB (86.21%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.java
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.java
@@ -1,4 +1,29 @@
 class Solution {
+
+    // Time Complexity: O(N)
+    public boolean checkDistances(String s, int[] distance) {
+        int[] firstSeen = new int[26];
+        Arrays.fill(firstSeen, -1);
+
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+
+            if (firstSeen[idx] == -1) {
+                firstSeen[idx] = i;
+            } else {
+                int actualIndex = i - firstSeen[idx] - 1;
+                if (distance[idx] != actualIndex) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    
+
+    /*
+    Time Complexity: O(N^2)
     public boolean checkDistances(String s, int[] distance) {
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
@@ -22,4 +47,6 @@ class Solution {
 
         return true;
     }
+
+    */
 }
